ef30c69f3701c1e7c84657302130d30e8fb5769a
milan <acmilanpooh@gmail.com>
2021-02-03 00:39:23 +0900

    新規ページ　コントローラー　作成

---
+++ portfolio/app/Http/Controllers/AppController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AppController extends Controller
+{
+    public function index() {
+        return view('app');
+    }
+}
